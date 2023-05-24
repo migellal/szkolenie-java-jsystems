f301cb0e7bb93a55eebf1f15643729dd09c62ed4
migellal <mgellert@jsystems.pl>
2023-05-24 13:00:20 +0000

    dzien 3 - kolekcje i iteracje

--- src/dzien3/Kolekcje.java
+++ src/dzien3/Kolekcje.java
@@ -6,10 +6,8 @@ import dzien3.generyki.OperationResult;
 import dzien3.generyki.Pudelko;
 import dzien3.ksiegowosc.Faktura;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class Kolekcje {
     public static void main(String[] args) {
@@ -61,11 +59,76 @@ public class Kolekcje {
         System.out.println(pudelkoNaNapisy.wezZPudelka());
 
         OperationResult<Faktura> fakturaOperationResult = zapisIOdczyt();
-        if(fakturaOperationResult.isSuccess()) {
+        if (fakturaOperationResult.isSuccess()) {
             System.out.println(fakturaOperationResult.getResult());
         } else {
             System.err.println(fakturaOperationResult.getErrorMessage());
         }
+
+        Set<String> set = new HashSet<>();
+        set.add("napis1");
+        set.add("napis4");
+        set.add("napis3");
+        System.out.println(set);
+        set.add("napis4"); // nie zostało dodane, ponieważ ten element istanił już wcześniej
+        System.out.println(set);
+        set.remove("napis1"); // zostało usunięte, nei istnieje opcja usunięcia po indexie
+        System.out.println(set);
+        System.out.println(set.size());
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(0, "zero");
+        map.put(1, "jeden");
+        System.out.println(map);
+        map.put(0, "zerozero"); // nadpisuje klucz, jeśli takowy istnieje
+        System.out.println(map);
+        map.putIfAbsent(1, "jedenjeden"); // dodaje klucz, jeśli go nie ma
+        System.out.println(map);
+        System.out.println(map.get(1));
+        map.put(3, "szukana");
+        map.put(4, "szukana");
+        System.out.println(map);
+
+        Set<Integer> keySet = map.keySet();
+        for (Integer key : keySet) {
+            String value = map.get(key);
+            if (value.equals("szukana")) {
+                System.out.println(key);
+            }
+        }
+
+        map.remove(4);
+        map.remove(3, "trzy"); // ten remove nie zadziała, ponieważ wartość jest inna
+        System.out.println(map);
+
+        System.out.println(map.size());
+
+        System.out.println(" -------------------- --- ");
+        for (Faktura fak : list) {// iterator fori, znajduje się powyżej
+            fak.policz();
+        }
+
+        System.out.println(" ----------------------- - ------");
+        for (String s : set) {
+            System.out.println(s);
+        }
+        set.forEach(new Consumer<String>() { // stare podejście
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+
+        set.forEach(s -> { // nowe podejście - lambda
+            System.out.println(s);
+        });
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) { // fori nie zadziała
+            System.out.println("Klucz: " + entry.getKey() + " Wartość: " + entry.getValue());
+        }
+
+        map.forEach((k, v) -> System.out.println("Klucz: " + k + " Wartość: " + v));
+
     }
 
     private static Krotka<Faktura, String> zlozonaOperacja() {
