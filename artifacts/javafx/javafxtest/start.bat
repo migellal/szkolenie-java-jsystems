b06499ada7cf3269a57133a0bdcb4cf707f70e0b
migellal <mgellert@jsystems.pl>
2023-05-26 12:56:28 +0000

    dzien 5 - artefakty

---
+++ artifacts/javafx/javafxtest/start.bat
@@ -0,0 +1,3 @@
+chcp 65001
+C:\Users\localadmin\.jdks\liberica-11.0.19\bin\javaw -jar C:\Users\localadmin\IdeaProjects\artifacts\javafx\javafxtest\javafxtest.jar
+PAUSE
---
+++ artifacts/start gpt.bat
@@ -0,0 +1,3 @@
+chcp 65001
+C:\Users\localadmin\.jdks\openjdk-20.0.1\bin\java -jar C:\Users\localadmin\IdeaProjects\artifacts\szkolenie-java-jsystems.jar
+PAUSE
