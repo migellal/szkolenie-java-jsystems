997e8a8c8e88499aca7e8ebf7851484b093943af
migellal <mgellert@jsystems.pl>
2023-05-26 09:25:28 +0000

    dzien 5 - baza danych, tworzenie rekordow

--- src/dzien5/baza/BazaDanych.java
+++ src/dzien5/baza/BazaDanych.java
@@ -1,8 +1,11 @@
 package dzien5.baza;
 
+import dzien5.baza.model.Employee;
 import dzien5.baza.service.EmployeeService;
 
 import java.sql.Connection;
+import java.util.Date;
+
 public class BazaDanych {
 
     private static final EmployeeService empService = new EmployeeService();
@@ -11,5 +14,11 @@ public class BazaDanych {
         System.out.println(connection);
 
         System.out.println(empService.findAll());
+
+        Employee employee = new Employee(300L, "Pracownik", "Pracujacy", "ppracujacy",
+                "123.123", new Date(), "IT_PROG", 15000.0, 0.0, 100L, 60L
+        );
+
+        empService.createEmployee(employee);
     }
 }
--- src/dzien5/baza/service/EmployeeService.java
+++ src/dzien5/baza/service/EmployeeService.java
@@ -3,6 +3,7 @@ package dzien5.baza.service;
 import dzien5.baza.Connector;
 import dzien5.baza.model.Employee;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,6 +42,30 @@ public class EmployeeService {
             throw new RuntimeException(e);
         }
         return list;
+    }
 
+    public void createEmployee(Employee employee) {
+        String sql = "insert into employees (EMPLOYEE_ID,FIRST_NAME,LAST_NAME,EMAIL," +
+                "PHONE_NUMBER,HIRE_DATE,JOB_ID,SALARY,COMMISSION_PCT," +
+                "MANAGER_ID,DEPARTMENT_ID) values (?,?,?,?,?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = Connector.getConnection().prepareStatement(sql);
+            ps.setLong(1, employee.getEmployeeId());
+            ps.setString(2, employee.getFirstName());
+            ps.setString(3, employee.getLastName());
+            ps.setString(4, employee.getEmail());
+            ps.setString(5, employee.getPhoneNumber());
+            ps.setDate(6, new java.sql.Date(employee.getHireDate().getTime()));
+            ps.setString(7, employee.getJobId());
+            ps.setDouble(8, employee.getSalary());
+            ps.setDouble(9, employee.getCommissionPct());
+            ps.setLong(10, employee.getManagerId());
+            ps.setLong(11, employee.getDepartmentId());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
